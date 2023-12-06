0874da7384f4cdff86cff33419b795148d485cd9
Abramov Iurii <abtank@bk.ru>
2023-12-06 11:53:56 +0300

    анотирование сборных полей и сортировака по ним в админке

--- NewsStudyRTK/news/admin.py
+++ NewsStudyRTK/news/admin.py
@@ -1,5 +1,7 @@
 ﻿# регистрация моделей в панеле админа
 from django.contrib import admin
+from django.db.models import Count
+from django.db.models.functions import Length
 
 from .models import *
 
@@ -7,15 +9,36 @@ from .models import *
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     ordering = ['-dt_public', 'title', 'author']
-    list_display = ['id', 'title', 'author', 'dt_public']
-    list_filter = ['tags', 'author', 'dt_public']
+    list_display = ['id', 'title', 'symbols_count', 'author', 'dt_public']
+    list_filter = ['tags', 'author', 'category', 'dt_public']
     list_display_links = ['id']
     list_editable = ['title', 'author']
     # readonly_fields = ['author']
     # prepopulated_fields = {"slug": ('title',)}
+    list_per_page = 10
+
+    @admin.display(description="Длинна", ordering='_text_symbols')
+    def symbols_count(self, article: Article):
+        return f"Символов {len(article.text)}"
+
+    # сортировка по symbols_count
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(_text_symbols=Length('text'))
+        return queryset
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
+    list_display = ['title', 'status', 'tag_count']
     list_filter = ['title', 'status']
+
+    @admin.display(description="Новостей", ordering='tag_count')
+    def tag_count(self, object):
+        return f"Новостей {object.tag_count}"
+
+    # сортировка по symbols_count
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(tag_count=Count('article'))
+        return queryset
